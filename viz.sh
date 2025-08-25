fa92d9e9282bae7d01bf483f718dd55cc0cb95df
wr1 <8971152+wr1@users.noreply.github.com>
2025-08-25 11:20:47 +0200

    Add new file viz.sh

---
+++ viz.sh
@@ -0,0 +1,22 @@
+
+
+# fold the current project
+cfold fold 
+clear 
+
+# fold the current project, but only the documentation 
+cfold fold -d doc
+clear 
+
+# fold a set of files
+cfold fold src/cfold/*/*py
+clear
+
+
+
+
+
+
+
+
+
